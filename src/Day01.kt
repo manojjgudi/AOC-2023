549014ef17b880492ec27c48364fdcef6568dce6
mgudi <mgudi@workfusion.com>
2023-12-01 11:30:47 +0530

    Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,12 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+    fun part1(input: List<String>): Int =
+        input.map { it.toList() }.sumOf { list -> (list.first { it.isDigit() } - '0') * 10 + (list.last { it.isDigit() } - '0') }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun part2(input: List<String>): Int =
+        input.map { it.toList() }.sumOf { list -> (list.first { it.isDigit() } - '0') * 10 + (list.last { it.isDigit() } - '0') }
 
-    // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
+    part1(testInput).println()
     val input = readInput("Day01")
-    part1(input).println()
     part2(input).println()
 }
