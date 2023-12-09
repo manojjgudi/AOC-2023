5f2fee0125fbbc3a8469639d14011eef07d7059d
mgudi <mgudi@workfusion.com>
2023-12-09 11:49:18 +0530

    Day09

--- src/Day09.kt
+++ src/Day09.kt
@@ -1,14 +1,14 @@
 fun main() {
     fun part1(input: List<String>): Long {
-        fun backTrack(list: List<Long>): Long =
-            if (list.all { it == 0L }) 0 else list.last() + backTrack(list.zipWithNext { i, j -> j - i }.toList())
-        return input.map { it.split(" ").map(String::toLong) }.map { backTrack(it) }.reduce { acc, sum -> acc + sum }
+        fun recurseTillZero(list: List<Long>): Long =
+            if (list.all { it == 0L }) 0 else list.last() + recurseTillZero(list.zipWithNext { i, j -> j - i }.toList())
+        return input.map { it.split(" ").map(String::toLong) }.map { recurseTillZero(it) }.reduce { acc, sum -> acc + sum }
     }
 
     fun part2(input: List<String>): Long {
-        fun backTrack(list: List<Long>): Long =
-            if (list.all { it == 0L }) 0 else list.first() - backTrack(list.zipWithNext { i, j -> j - i }.toList())
-        return input.map { it.split(" ").map(String::toLong) }.map { backTrack(it) }.reduce { acc, sum -> acc + sum }
+        fun recurseTillZero(list: List<Long>): Long =
+            if (list.all { it == 0L }) 0 else list.first() - recurseTillZero(list.zipWithNext { i, j -> j - i }.toList())
+        return input.map { it.split(" ").map(String::toLong) }.map { recurseTillZero(it) }.reduce { acc, sum -> acc + sum }
     }
 
     val testInput = readInput("Day09_test")
